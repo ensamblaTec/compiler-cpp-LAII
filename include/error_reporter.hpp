47f24b226ecbbf24d0a5cf12b063c55ca21f8c94
ensamblatec <net.git.ketza@gmail.com>
2025-04-14 12:10:54 -0600

    add struct to report errors

---
+++ include/error_reporter.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+struct ErrorInfo {
+    std::string message;
+    int line;
+    int column;
+};
+
+class ErrorReporter {
+public:
+    static ErrorReporter& getInstance();
+
+    void report(const std::string& message, int line, int column);
+    void printSummary() const;
+    void clear();
+    bool hasErrors() const;
+
+private:
+    std::vector<ErrorInfo> errors;
+    ErrorReporter() = default;
+};
+
