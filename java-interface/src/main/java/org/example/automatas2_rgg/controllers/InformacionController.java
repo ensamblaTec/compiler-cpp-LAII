6f525eddd341ae9460729c9c6b1c6f3b4323c3b4
Martina Daniela <19030013@itcelaya.edu.mx>
2025-05-24 01:25:58 -0600

    Agrego presentacion y gramatica

--- java-interface/src/main/java/org/example/automatas2_rgg/controllers/InformacionController.java
+++ java-interface/src/main/java/org/example/automatas2_rgg/controllers/InformacionController.java
@@ -26,11 +26,28 @@ public class InformacionController {
             FXMLLoader loader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
             Parent root = loader.load();
 
+            // Recuperar el Stage actual desde el menú
             Stage stage = (Stage) ((MenuItem) event.getSource()).getParentPopup().getOwnerWindow();
-            stage.setScene(new Scene(root));
+
+            // Crear la escena con dimensiones fijas
+            Scene scene = new Scene(root, 800, 600);
+
+            // Si deseas aplicar un CSS, descomenta esto y actualiza la ruta:
+            // scene.getStylesheets().add(HelloApplication.class.getResource("css/estilos.css").toExternalForm());
+
+            // Establecer la escena y mantener el título y tamaño fijo
+            stage.setScene(scene);
+            stage.setTitle("Hello!");
+            stage.setResizable(false); // Esto evita que el usuario pueda cambiar el tamaño
             stage.show();
+
+            // Opcional: Obtener el controlador, si necesitas pasarle datos
+            HelloController controller = loader.getController();
+            controller.setStage(stage);
+
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
 }
